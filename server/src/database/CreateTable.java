3c7552992b04142149a94b61925f4973755279df
Julia Björnberg <60344508+Bjoernberg@users.noreply.github.com>
2020-04-24 10:22:40 +0200

    Insert database classes

---
+++ server/src/database/CreateTable.java
@@ -0,0 +1,47 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class CreateTable {
+    /**
+     * MySQL connection to database. (do not touch)
+     * @return connection to database
+     */
+    private Connection connect() {
+
+        try {
+            String url = "jdbc:mysql://localhost:3306/darenshare?serverTimezone=UTC";
+            Connection con = DriverManager.getConnection(url, "root", "julia");
+            System.out.println(con);
+            System.out.println(con.getClientInfo().toString());
+            return con;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connect();
+        //return con;
+    }
+
+
+    public void createTable() throws SQLException {
+        Connection con = this.connect();
+        String sql = "CREATE TABLE Dare (Dareid integer AUTO_INCREMENT, ObjectiveType text, Objective text, " +
+                "ScopeType text, ScopeLength text,Start text, End text, Participants integer, primary key (Dareid));";
+        //  String sql1 ="CREATE TABLE User(UserID integer AUTO_INCREMENT, UserName text, Friends text, primary key (UserID));";
+        //String sql2 ="CREATE TABLE Participants(PartID integer AUTO_INCREMENT, UserId integer, DareId integer, " +
+        //     "primary key (PartID), foreign key (UserId) references darenshare.user(UserID), foreign key (DareId)references darenshare.dare(Dareid))";
+        //    String sql="ALTER TABLE darenshare.dare ADD FOREIGN KEY (Participants) references darenshare.participants(PartID)";
+        Statement statement = con.createStatement();
+        // statement.execute(sql);
+        // statement.execute(sql1);
+        statement.execute(sql);
+    }
+
+    public static void main(String[] args) throws SQLException {
+        CreateTable table = new CreateTable();
+        //table.createTable();
+    }
+}
